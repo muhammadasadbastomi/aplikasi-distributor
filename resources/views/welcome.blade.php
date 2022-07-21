d3db134d8c8f55746cf77e02efe30a5d1c70242d
Saukani53 <muhammadsaukani.jrj@gmail.com>
2022-07-21 20:20:22 +0700

    tes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <!-- Required meta tags -->
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{asset('assets/images/favicon-32x32.png')}}" type="image/png" />
